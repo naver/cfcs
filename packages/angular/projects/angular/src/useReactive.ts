59f503c171e2b529009415bce57652df2948240b
arturovt <arthurandrosovich@gmail.com>
2023-03-14 22:46:13 +0200

    fix(angular): adopt instance events to be scheduled in Angular zone

--- packages/angular/projects/angular/src/useReactive.ts
+++ packages/angular/projects/angular/src/useReactive.ts
@@ -3,16 +3,37 @@
  * Copyright (c) 2022-present NAVER Corp.
  * MIT license
  */
-import { EventEmitter } from "@angular/core";
-import { ReactiveSubscribe, ReactiveAdapterParam, camelize, adaptReactive, ReactiveState } from "@cfcs/core";
+import { EventEmitter, NgZone, inject } from '@angular/core';
+import {
+  ReactiveSubscribe,
+  ReactiveAdapterParam,
+  camelize,
+  adaptReactive,
+  ReactiveState,
+} from '@cfcs/core';
 
 export function useReactive<
   Instance extends ReactiveSubscribe<Record<string, any>>,
   State extends Record<string, any> = ReactiveState<Instance>,
   Methods extends keyof Partial<Instance> = any,
   Props = any,
-  Events extends Record<string, any> = {},
->(self: State, reactiveAdapter: ReactiveAdapterParam<Instance, State, Methods, Props, Events>, props?: () => Props) {
+  Events extends Record<string, any> = {}
+>(
+  self: State,
+  reactiveAdapter: ReactiveAdapterParam<
+    Instance,
+    State,
+    Methods,
+    Props,
+    Events
+  >,
+  props?: () => Props,
+  // The `NgZone` is an optional parameter so the implementation doesn't bring any breaking changes.
+  // Required to be provided so events are dispatched within the correct execution context.
+  ngZone?: NgZone
+) {
+  ngZone ||= tryToInjectNgZone();
+
   const adaptResult = adaptReactive(reactiveAdapter, props);
   const reactiveState = adaptResult.state();
   const names = Object.keys(reactiveState);
@@ -26,7 +47,7 @@ export function useReactive<
     (self as any)[name] = methods[name];
   }
 
-  events.forEach(name => {
+  events.forEach((name) => {
     (self as any)[camelize(`ngx ${name as string}`)] = new EventEmitter();
   });
   let callbacks: Array<(...args: any[]) => void> = [];
@@ -43,9 +64,25 @@ export function useReactive<
       });
     });
 
-    callbacks = events.map(name => {
+    callbacks = events.map((name) => {
       const callback = (...args: any[]) => {
-        ((self as any)[camelize(`ngx ${name as string}`)] as EventEmitter<any>).emit(...args);
+        const property = camelize(`ngx ${name as string}`);
+        const emitter = (self as any)[property] as EventEmitter<any>;
+
+        if (ngZone && (emitter.observed || emitter.observers.length > 0)) {
+          // The `mounted()` should naturally be called outside of the Angular
+          // zone, meaning all of the setups of third-party packages are done
+          // w/o telling Angular that there's any asynchronous work happening or
+          // going to be scheduled in the future. This means all events will be
+          // triggered outside of the Angular zone too. We'd want to re-enter the
+          // Angular zone only if there're any `EventEmitter` observers. Given the
+          // `EventEmitter` is `ngxMyEvent = new EventEmitter()`. The `ngxMyEvent`
+          // has no observers until developers set up event listeners in templates,
+          // for instance, `<div myDirective (ngxMyEvent)="onMyEvent()"></div>`.
+          ngZone.run(() => emitter.emit(...args));
+        } else {
+          emitter.emit(...args);
+        }
       };
 
       adaptResult.on(name as any, callback);
@@ -53,6 +90,7 @@ export function useReactive<
     });
     adaptResult.init();
   }
+
   function destroy() {
     events.forEach((name, i) => {
       adaptResult.off(name as any, callbacks[i]);
@@ -65,4 +103,16 @@ export function useReactive<
     mounted,
     destroy,
   };
+}
+
+function tryToInjectNgZone(): NgZone | undefined {
+  try {
+    // The `inject` may throw an error since its implementation differs between Angular versions
+    // and the injection context where the `inject` function is being called.
+    // The `useReactive` is usually called in directive or component constructors. Component constructors
+    // are injection contexts by themselves.
+    return inject(NgZone);
+  } catch {
+    return undefined;
+  }
 }
